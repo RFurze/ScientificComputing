d379bb08ec82e335d018163d0242301b7abe5c3e
RFurze <py07rf@leeds.ac.uk>
2023-11-18 11:13:14 +0000

    Update gradient descent function with momentum modification

--- momgraddesc.py
+++ momgraddesc.py
@@ -1,35 +1,41 @@
-def momgraddesc(fnon, jac, x0, gamma, tol, maxk, *fnonargs):
+def myGradientDescentMom(fnon, jac, x0, gamma, tol, maxk, *fnonargs):
     # fnon     - name of the nonlinear function f(x)
     # jac      - name of the Jacobian function J(x)
+    # gamma    - momentum factor
     # x0       - initial guess for the solution x0
-    # tol      - stopping tolerance for Newton's iteration
+    # tol      - stopping tolerance
     # maxk     - maximum number of Newton iterations before stopping
-    # fnonargs - optional arguments that will be passed to the nonlinear function (useful for additional function parameters)
+    # fnonargs - optional arguments
+    
     k = 0
     x = x0
     lmb = 0.01
-    iterates = np.array(x)
-    delta0 = 0
+
+    # Note: handling of xArray is updated out of personal preference
+    # - xArray.append is replaced with hstack further down
+    xArray = np.array(x)
     delta = 0
 
     F = eval(fnon)(x, *fnonargs)
 
     print(" k    f(xk)")
 
-    # Main Newton loop
-    while np.linalg.norm(F) > tol and k < maxk:
+    # Main gradient descent loop
+    # Update the stopping criteria to stop when k == maxk
+    # (i.e account for zero based indexing)
+    while np.linalg.norm(F, 2) > tol and k < maxk:
         # Evaluate Jacobian matrix
         J = eval(jac)(x, *fnonargs)
 
-        # Calculate the direction - the previous value of delta has been incorporated with the momentum factor gamma
-        delta = delta * gamma + 2 * np.transpose(J) @ F
+        # Take gradient descent step
+        # UPDATE: calculate a new gradient descent step using
+        # the momentum modification
+        delta = delta * gamma + 2 * np.dot(np.transpose(J), F)
 
         x = x - lmb * delta
 
         F = eval(fnon)(x, *fnonargs)
-        # the current value of delta is stored over he previous value for the next iteration.
-        # delta0 = delta
-        iterates = np.hstack([iterates, x])
+        xArray = np.hstack([xArray, x])
         k += 1
         print("{0:2.0f}  {1:2.2e}".format(k, np.linalg.norm(F)))
 
@@ -38,4 +44,4 @@ def momgraddesc(fnon, jac, x0, gamma, tol, maxk, *fnonargs):
     else:
         print("Converged to ")
         print(x)
-    return iterates
+    return xArray
